e3d5d520aec6922fc295e4adb1f6d76fabbd16f7
Michelle <michellechen720@gmail.com>
2021-10-22 15:24:27 -0400

    Add project files.

---
+++ HDS_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HDS_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
