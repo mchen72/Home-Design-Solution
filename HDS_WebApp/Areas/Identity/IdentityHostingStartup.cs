e36bf625bcd98596e747f23e8595d523c8e1a95f
Michelle <michellechen720@gmail.com>
2021-12-01 12:16:11 -0500

    Login pages

---
+++ HDS_WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HDS_WebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HDS_WebApp.Areas.Identity.IdentityHostingStartup))]
+namespace HDS_WebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
